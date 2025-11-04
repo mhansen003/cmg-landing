f8b5e8858a226b68f8602ebcb21fcfe9a1e5605c
mhansen003 <markdfm@gmail.com>
2025-11-04 13:03:50 -0800

    Make landing page publicly accessible

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { verifyAuthToken, AUTH_CONFIG } from './lib/auth-jwt';
 
 // Routes that don't require authentication
 const PUBLIC_ROUTES = [
+  '/',
   '/login',
   '/api/auth/send-otp',
   '/api/auth/verify-otp',
